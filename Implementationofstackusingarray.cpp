02fe453391bd6ceb897fe91019d7720cddac063a
Abhay Patel <93342343+patelabhay12@users.noreply.github.com>
2021-11-10 22:00:32 +0530

    Add files via upload

---
+++ Implementationofstackusingarray.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Stack{
+    int size;
+    int top;
+    int *s;
+};
+
+void create(struct Stack *st){
+    printf("Enter size of stack ");
+    scanf("%d",&st->size);
+    st->top=-1;
+    st->s=(int *)malloc(st->size*sizeof(int));
+    
+
+}
+void Display(struct Stack *st)
+{
+    int i;
+    for(i=st->top ;i>=0;i--){
+        printf("%d ",st->s[i]);
+    }
+    printf("\n");
+}
+
+void push(Stack *st ,int x){
+    if(st->top==st->size-1){
+        printf("stack over flow :");
+    }
+    else{
+        st->top++;
+        st->s[st->top]=x;
+
+    }
+
+}
+int  pop(struct Stack *st){
+    int x=-1;
+    if(st->top==-1){
+        printf("Stack Underflow\n");
+    }
+    else{
+        x=st->s[st->top--];
+    }
+    return x;
+    
+}
+
+
+int main(){
+    struct Stack st;
+    create(&st);
+    push(&st,10);
+    push(&st,20);
+    push(&st,30);
+    push(&st,40);
+    Display(&st);
+
+    
+}
